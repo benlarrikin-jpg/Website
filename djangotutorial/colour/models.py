635c31d420f96c475dd1d6230004189f4847b4e5
Igwe Hanna <igwehannahvirginie@gmail.com>
2025-09-02 16:16:16 +0100

    created an app

---
+++ djangotutorial/colour/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+#Create your models here.
+class Food(models.Model):
+    Name=models.CharField(max_length=200)
+
+    price=models.FloatField(max_length=20,default=0)
+    picture=models.ImageField(upload_to='images/')
+
+    description=models.TextField(max_length=100)
---
+++ djangotutorial/djangotutorial/models.py
@@ -0,0 +1,13 @@
+# from django.db import models
+# #a website where users can order food
+# class Food:
+#     Name
+#     price
+#     picture
+#     description 
+# class car:
+#     my_car = car()
+#     wheels = 4
+#     def __init__(self, make, model):
+#         self.make = make  # Instance attribute
+#         self.model = model  # Instance attribute
